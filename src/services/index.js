a673ea30d8e488233ceaca324b8b9b9ab2a74656
yiqizouba <52811524+yiqizouba@users.noreply.github.com>
2023-01-06 20:09:20 +0800

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -519,11 +519,11 @@ export const getBirthdayMessage = (festivals) => {
         } else {
           age = selfDayjs().year() - item.year - 1
         }
-
-        if (item.diffDay === 0) {
-          message = `今天是 ${item.name} 的${age && item.isShowAge ? `${(item.useLunar ? 1 : 0) + age}岁` : ''}生日哦，祝${item.name}生日快乐！`
+       
+ if (item.diffDay === 0) {
+          message = `今天是 ${item.name} 的${age && item.isShowAge ? `${(item.useLunar ? 1 : 0) + age}岁` : ''}${item.type.indexOf('*') !== -1 ? '阴历' : '公历'}生日哦，祝${item.name}生日快乐！`
         } else {
-          message = `距离 ${item.name} 的${age && item.isShowAge ? `${age + 1}岁` : ''}生日还有${item.diffDay}天`
+          message = `距离 ${item.name} 的${age && item.isShowAge ? `${age + 1}岁` : ''}${item.type.indexOf('*') !== -1 ? '阴历' : '公历'}生日还有${item.diffDay}天`
         }
       }
 
